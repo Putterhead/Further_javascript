6a328da43ec7707780c15336d2de3f74186bb8d5
PeteGH <peter@grant-hay.eu>
2017-02-14 16:04:56 +0100

    adds note-model.js et al;

---
+++ spec/note-list-tests.js
@@ -0,0 +1,11 @@
+
+(function() {
+	notelist = new NoteList();
+	assert.isEmpty(notelist.getNotes());
+})();
+
+(function() {
+	notelist = new NoteList();
+	notelist.addNote("this is a note");
+	assert.isNotEmpty(notelist.getNotes());
+})();
